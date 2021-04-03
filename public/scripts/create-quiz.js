276fe167c6be197217237efb699e75bacf653426
Darryn-Wong <Darryn-Wong@users.noreply.github.com>
2021-04-03 04:47:30 -0700

    update quiz

--- public/scripts/create-quiz.js
+++ public/scripts/create-quiz.js
@@ -1,60 +1,34 @@
 
-// class Point {
-//     constructor(category, point) {
-//         this.category = category;
-//         this.point = point;
-//     }
-// }
-// class Qna {
-//     constructor(q, a) {
-//         this.q = q;
-//         this.a = a;
-//     }
-//     toArray() {
-//         let data = []
-//         let question, answer = {}
-//         question = { q: this.q }
-//         answer =
-//             data.push(question)
-//         data.push(answer)
-//         data(this.a)
-//         return JSON.parse(JSON.stringify(data));
-//     }
-// }
-// class A {
-//     constructor(a) {
-//         this.a;
-//     }
-// }
-// class Quiz {
-//     constructor(title, qna, category) {
-//         this.title = title;
-//         this.qna = qna;
-//         this.category = category;
-//     }
-// }
-// let qnaList = []
-//create category point system
-
-// let answer1 = [{ q: 'ok', 'hobo': 2 }, { q: 'im ok', 'rich': 3 }, { q: 'gud', 'rich': 3, 'hobo': 2 }, { q: 'gud', 'rich': 3, 'hobo': 1 }];
-// let answer2 = [{ q: 'happppppyyyyy', 'omega rich': 150 }, { q: 'not happy', 'hobo': 100 }, { q: 'happy', 'rich': 13, 'hobo': 2 }, { q: 'maybe', 'rich': 3, 'hobo': 5 }];
-// let qna1 = new Qna('you happy?', answer1);
-// let qna2 = new Qna('you happy!????', answer2);
-// qnaList.push(qna1)
-// qnaList.push(qna2)
-
-
-// let q = new Quiz(`Darryn's fav quiz`, qnaList, catList);
-// console.log(q)
-// console.log(qna1.toArray())
-
-
-// TODO
-// think of a good way to turn the above class obj into pure java obj without nested loops (firebase doesnt like it)
-
 // working example
+
+const quiz = {
+    qna: {},
+    appendQna: function (qna) {
+        if (typeof this.index === 'undefined') {
+            this.index = 0;
+        }
+        this.qna[this.index] = qna;
+        this.index = this.index + 1;
+        return this;
+    },
+    setCatList: function (catList) {
+        this.catList = catList;
+        return this;
+    },
+};
+
 let qna1 = [{ q: 'you happy?' }, { a: 'ok', 'hobo': 2 }, { a: 'im ok', 'rich': 3 }, { a: 'gud', 'rich': 3, 'hobo': 2 }, { a: 'gud', 'rich': 3, 'hobo': 1 }];
+let qna2 = [{ q: 'you happppppyyyyy?' }, { a: "omega happy", 'omega rich': 100 }, { a: 'not happy', 'hobo': 100 }, { a: 'happy', 'rich': 13, 'hobo': 2 }, { a: 'maybe', 'rich': 3, 'hobo': 5 }];
+
+let qna = [{ 1: qna1 }, { 2: qna2 }]
 let catList = ['hobo', 'rich', 'omega rich']
+const q = Object.create(quiz);
+
+q.title = "Example one";
+q.catList = catList;
+q.appendQna(qna1).appendQna(qna2)
+console.log(q.title, q.qna, q.catList)
+// saveQuiz(q.title, q.qna, q.catList);
 
 function saveQuiz(title, qna, category) {
     // Add a new message entry to the database.
@@ -68,9 +42,6 @@ function saveQuiz(title, qna, category) {
         console.error('Error writing new message to database', error);
     });
 }
-// saveQuiz("Darryn's fav", qna1, catList);
-
-
 
 // console.log(getUserName())
 setTimeout(() => { console.log(getUserName()) }, 5000);
--- public/scripts/loading.js
+++ public/scripts/loading.js
@@ -2,9 +2,5 @@ function removeLoading() {
     $('#preload').remove();
 }
 function setBodyVisible() {
-    // document.getElementsByTagName("body").style.visibility = "visible";
-    // signOutButtonElement.setAttribute('hidden', 'true');
-    // $('body').removeAttribute('hidden');
-    // $("body").attr("visibility", "hasdas");
     document.getElementById("body").style.visibility = "visible";
 }
